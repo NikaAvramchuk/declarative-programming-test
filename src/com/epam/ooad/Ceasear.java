198fce4f07665cc92c2f5416dc55046c98c0136d
Nika <nika.avramchuk@gmail.com>
2020-09-11 00:03:45 +0200

    change part of the code responsible for encryption usin declarative paradigm

---
+++ src/com/epam/ooad/Ceasear.java
@@ -0,0 +1,78 @@
+package com.epam.ooad;
+
+import java.util.Scanner;
+
+public class Ceasear {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Ave com.epam.oopconcept.Caesar! Morituri te salutant!");
+        String input, output;
+        do {
+            System.out.println("Would you like to encrypt or decrypt a message?");
+            input = in.nextLine();
+            if (input.equals("encrypt")) {
+                input = askForPlainTextMessage(in);
+                output = encryptMessage(input);
+                outputEncryptedMessage(output);
+                break;
+            } else if (input.equals("decrypt")) {
+                System.out.println("Enter a message to decrypt: ");
+                input = in.nextLine();
+                output = "";
+                for (int i = 0; i < input.length(); i++) {
+                    char letter = input.charAt(i);
+                    if ('A' <= letter && letter <= 'Z') {
+                        if (letter - 3 >= 'A') {
+                            output = output + (char)(letter - 3);
+                        } else {
+                            output = output + (char)(letter - 3 + 26);
+                        }
+                    } else {
+                        output = output + letter;
+                    }
+                }
+                System.out.println("Decrypted message: ");
+                System.out.println(output);
+                break;
+            } else {
+                System.out.println("Tertium non datur!");
+            }
+        } while (true);
+        System.out.println("Vale!");
+    }
+
+    private static void outputEncryptedMessage(String output) {
+        System.out.println("Encrypted message: ");
+        System.out.println(output);
+    }
+
+    private static String encryptMessage(String input) {
+        String output;
+        output = "";
+        output = encryptSingleCharacter(input, output);
+        return output;
+    }
+
+    private static String encryptSingleCharacter(String input, String output) {
+        for (int i = 0; i < input.length(); i++) {
+            char letter = input.charAt(i);
+            if ('A' <= letter && letter <= 'Z') { // validation
+                if (letter + 3 <= 'Z') {
+                    output = output + (char)(letter + 3);
+                } else {
+                    output = output + (char)(letter + 3 - 26);
+                }
+            } else {
+                output = output + letter;
+            }
+        }
+        return output;
+    }
+
+    private static String askForPlainTextMessage(Scanner in) {
+        String input;
+        System.out.println("Enter a message to encrypt: ");
+        input = in.nextLine();
+        return input;
+    }
+}
